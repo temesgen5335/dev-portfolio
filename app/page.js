34be3cc288c151c8f925be53c5f9157e2c0c688d
temesgen5335 <temesgen5335@gmail.com>
2025-12-09 20:13:59 +0300

    update theme and folder structure

---
+++ app/page.js
@@ -0,0 +1,25 @@
+import HeroSection from "../components/HeroSection";
+import Navbar from "../components/Navbar";
+import AboutSection from "../components/AboutSection";
+import ProjectsSection from "../components/ProjectsSection";
+import EmailSection from "../components/EmailSection";
+import Footer from "../components/Footer";
+import AchievementsSection from "../components/AchievementsSection";
+import SpacePortfolio from "../components/newHero";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col bg-[#0D0F1A]">
+      <div className=" mx-auto">
+        <HeroSection />
+        {/* <SpacePortfolio /> */}
+
+        {/* <AchievementsSection /> */}
+        <AboutSection />
+        <ProjectsSection />
+        <EmailSection />
+      </div>
+      <Footer />
+    </main>
+  );
+}
